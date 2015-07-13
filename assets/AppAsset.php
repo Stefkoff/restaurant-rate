a3711322e9ad1ac86a08386a80b9da2cdeef414c
Stefkoff <scava_comp>
2015-07-13 15:51:18 +0300

    Added font-awesome and Yii updated

--- assets/AppAsset.php
+++ assets/AppAsset.php
@@ -22,6 +22,7 @@ class AppAsset extends AssetBundle
     );
      public $depends = [
         'yii\web\YiiAsset',
+         '\rmrevin\yii\fontawesome\AssetBundle'
 //        'yii\bootstrap\BootstrapAsset',
 //        'yii\web\JqueryAsset'
     ];
