417b678d190b490fb4ba75500a534a47b91885b5
kyuchukovv <kyuchukovv@gmail.com>
2015-07-15 19:49:25 +0300

    Edite Profile

--- views/user/profile.php
+++ views/user/profile.php
@@ -6,27 +6,27 @@ use yii\helpers\Url;
 $this->title = 'Профил';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
+<div class="row-fluid">
+    <div class="user-profile">
+        <div class="row">
+            <div class="span6">
+                <div class="row-fluid">
+                    <div class="span12">
+                        <h1>
+                            <b><?=$user->firstname." ".$user->lastname?></b>
 
-<div class="user-profile">
-    <div class="row">
-        <div class="span6">
-            <div class="row-fluid">
-                <div class="span12">
-                    <h1>
-                        Вашият <?= Html::encode($this->title) ?>
-                    </h1>
-                    <i><?=Html::a('Редактирай', Url::toRoute(['user/edit', 'id' => $user->id]))?></i>
+
+                        </h1>
+                        <div class="span6"> Email: <b><?=$user->email;?></b></div>
+                        <i class="fa fa-beer"></i>
+                        <i><?=Html::a('Редактирай', Url::toRoute(['user/edit', 'id' => $user->id]))?></i>
+                    </div>
+
+
                 </div>
 
-
-        </div>
-            <div class="row">
-                <div class="span6">
-                    Имена: <b><?=$user->firstname." ".$user->lastname?></b>
-
-                </div>
-                <div class="span6"> Email: <b><?=$user->email;?></b></div>
             </div>
+        </div>
     </div>
 </div>
 
